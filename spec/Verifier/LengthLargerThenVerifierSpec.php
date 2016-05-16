2f504840aaddf2a3e25200c4c7bdd45ce31677bd
Simon Vocella <voxsim@gmail.com>
2016-05-16 19:07:33 +0200

    delete duplication in LengthLargerThenVerifierSpec

--- spec/Verifier/LengthLargerThenVerifierSpec.php
+++ spec/Verifier/LengthLargerThenVerifierSpec.php
@@ -12,21 +12,23 @@ class LengthLargerThenSpec extends ObjectBehavior
     const STRING_WITH_LENGTH_EQUAL_TO_THRESHOLD = 'a';
     const LARGE_STRING = 'aa';
 
-    function it_returns_false_with_string_shorter_than_threshold()
+    function let()
     {
         $this->beConstructedWith(self::THRESHOLD);
+    }
+
+    function it_returns_false_with_string_shorter_than_threshold()
+    {
         $this->verify(self::SHORT_STRING)->shouldReturn(false);
     }
 
     function it_returns_false_with_string_that_has_length_equal_to_threshold()
     {
-        $this->beConstructedWith(self::THRESHOLD);
         $this->verify(self::STRING_WITH_LENGTH_EQUAL_TO_THRESHOLD)->shouldReturn(false);
     }
 
     function it_returns_true_with_string_larger_than_threshold()
     {
-        $this->beConstructedWith(self::THRESHOLD);
         $this->verify(self::LARGE_STRING)->shouldReturn(true);
     }
 }
